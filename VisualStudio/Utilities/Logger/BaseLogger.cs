afeaa347f27428c85c03ce5d4fe19d47e4c892d5
The Illusion <20694721+Arkhorse@users.noreply.github.com>
2024-12-06 13:15:29 -0400

    add color

--- VisualStudio/Utilities/Logger/BaseLogger.cs
+++ VisualStudio/Utilities/Logger/BaseLogger.cs
@@ -15,5 +15,16 @@ namespace ComplexLogger
 		{
 			Melon<T>.Logger.Msg(message, parameters);
 		}
+
+		/// <summary>
+		/// The base log method with color
+		/// </summary>
+		/// <param name="message">The formated string to add as the message</param>
+		/// <param name="color">The color of the text to write</param>
+		/// <param name="parameters">Any additional params, note that this must be either a single item or an array <c>Write("", [object, object])</c></param>
+		protected void Write(string message, ConsoleColor color, params object[] parameters)
+		{
+			Melon<T>.Logger.Msg(color, message, parameters);
+		}
 	}
 }
